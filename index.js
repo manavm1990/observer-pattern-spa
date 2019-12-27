b1720c6dfe80af65b44f503fbd2a15a07936969b
Manav Misra <manavm@visionwebsoft.com>
2019-12-26 18:59:02 -0600

    :construction: Add <div>s for all components.
    https://www.npmjs.com/package/lodash.kebabcase

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import * as state from "./store";
-import { UserList } from "./components";
+import * as components from "./components";
 
-UserList.render(state.Users);
+import kebabCase from "lodash.kebabcase";
+
+// Create <div> for each component inside of root.
+document.getElementById("root").innerHTML = Object.keys(components)
+  .map(component => `<div id="${kebabCase(component)}"></div>`)
+  .join("");
